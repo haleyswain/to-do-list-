35e4269f9f02ff5dd56affe4ca9e82df1cd217a3
Haley Swain and Margie Nevarez <haley@>
2016-12-05 16:52:07 -0800

    sorted method

--- lib/task.rb
+++ lib/task.rb
@@ -1,9 +1,10 @@
 class Task
-  attr_reader(:description, :list_id)
+  attr_reader(:description, :list_id, :due_date)
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
     @list_id = attributes.fetch(:list_id)
+    @due_date =  attributes.fetch(:due_date)
   end
 
 
@@ -18,8 +19,9 @@ class Task
       #gathers descriptions for each of the tasks
       description = task.fetch("description")
       list_id = task.fetch("list_id").to_i()
+      due_date = task.fetch('due_date')
       #pushes new instance of task to the task array
-      tasks.push(Task.new({:description => description, :list_id => list_id}))
+      tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
     end
     tasks
   end
@@ -30,6 +32,17 @@ class Task
   define_method(:save) do
     #Uses PG gem to execute argument
       #Adds an input, description, to the description column in the tasks table
-    DB.exec("INSERT INTO tasks (description, list_id) VALUES ('#{@description}', #{@list_id});")
+    DB.exec("INSERT INTO tasks (description, list_id, due_date) VALUES ('#{@description}', #{@list_id}, '#{@due_date}');")
+  end
+  define_singleton_method(:sort) do
+    sorted_tasks_data= DB.exec("SELECT * FROM tasks ORDER BY due_date ASC;")
+    sorted_tasks = []
+    sorted_tasks_data.each() do |task|
+    due_date = task.fetch("due_date")
+    description = task.fetch("description")
+    list_id = task.fetch("list_id").to_i()
+    sorted_tasks.push(Task.new({:description => description, :list_id => list_id, :due_date => due_date}))
+  end
+    sorted_tasks
   end
  end
--- spec/task_spec.rb
+++ spec/task_spec.rb
@@ -3,7 +3,7 @@ require('spec_helper')
 describe(Task) do
   describe("#description") do
     it("lets you give it a description") do
-      test_task = Task.new({:description => "wash the lion", :list_id => 1})
+      test_task = Task.new({:description => "wash the lion", :list_id => 1, :due_date => '2016-05-01'})
       expect(test_task.description()).to(eq("wash the lion"))
     end
   end
@@ -14,22 +14,31 @@ describe(Task) do
   end
   describe("#==") do
     it("is the same task if it has the same description") do
-      task1 = Task.new({:description => "learn SQL", :list_id => 1})
-      task2 = Task.new({:description => "learn SQL", :list_id => 1})
+      task1 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-05-01'})
+      task2 = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-05-01'})
       expect(task1).to(eq(task2))
     end
   end
   describe('#save') do
     it('adds a task to the array of saved tasks') do
-      test_task = Task.new({:description => "learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "learn SQL", :list_id => 1, :due_date => '2016-05-01'})
       test_task.save()
       expect(Task.all()).to(eq([test_task]))
     end
   end
   describe("#list_id") do
     it("lets you read the list ID out") do
-      test_task = Task.new({:description => "Learn SQL", :list_id => 1})
+      test_task = Task.new({:description => "Learn SQL", :list_id => 1, :due_date => '2016-05-01'})
       expect(test_task.list_id()).to(eq(1))
+    end
+  end
+  describe("#sort") do
+    it("sort tasks by due date") do
+      test_task = Task.new({:description => "Apply for a tech job", :list_id => 1, :due_date => '2016-05-01'})
+      test_task2 = Task.new({:description => "finish project", :list_id => 2, :due_date => '2016-03-01'})
+      test_task.save()
+      test_task2.save()
+      expect(Task.sort()).to(eq([test_task2, test_task]))
     end
   end
 end
